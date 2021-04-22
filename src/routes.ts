1a65b9a448ef3d5874b7321fb1c7258f8d71c989
Ian Saft <devdoamanha@gmail.com>
2021-04-21 22:40:02 -0300

    Day 3

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
+import { MessagesController } from './controllers/messagesController';
 import { SettingsController } from './controllers/settingsController';
+import { UsersController } from './controllers/usersController';
 
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
-routes.post('/settings', settingsController.create)
+routes.post('/settings', settingsController.create);
+
+routes.post('/users', usersController.create);
+
+routes.post('/messages', messagesController.create);
+routes.get('/messages/:id', messagesController.showByUser);
 
 export { routes };
